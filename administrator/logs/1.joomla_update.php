973eb6aca1132ee160b30f341d87aa3dfcdeeea4
Bold New Media <support@boldnewmedia.com.au>
2023-03-09 22:09:13 +1100

    Updated Joomla => 3.10.11

---
+++ administrator/logs/1.joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-06-25 02:01:57 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-06-25T02:01:57+00:00	INFO ::1	update	Update started by user Super User (718). Old version is 3.10.8.
+2022-06-25T02:01:58+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.10.10-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20220625%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220625T020155Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=c0dd0c6615e035ad2de790bc81aba00c4687a5cb3d30f2f22917f1b128501eea.
+2022-06-25T02:02:03+00:00	INFO ::1	update	File Joomla_3.10.10-Stable-Update_Package.zip downloaded.
+2022-06-25T02:02:03+00:00	INFO ::1	update	Starting installation of new version.
+2022-06-25T02:02:09+00:00	INFO ::1	update	Finalising installation.
+2022-06-25T02:02:09+00:00	INFO ::1	update	Deleting removed files and folders.
+2022-06-25T02:02:10+00:00	INFO ::1	update	Cleaning up after installation.
+2022-06-25T02:02:10+00:00	INFO ::1	update	Update to version 3.10.10 is complete.
--- administrator/logs/joomla_update.php
+++ administrator/logs/joomla_update.php
@@ -1,14 +1,14 @@
 #
 #<?php die('Forbidden.'); ?>
-#Date: 2022-06-25 02:01:57 UTC
+#Date: 2023-03-09 11:07:57 UTC
 #Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
 
 #Fields: datetime	priority clientip	category	message
-2022-06-25T02:01:57+00:00	INFO ::1	update	Update started by user Super User (718). Old version is 3.10.8.
-2022-06-25T02:01:58+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.10.10-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20220625%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220625T020155Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=c0dd0c6615e035ad2de790bc81aba00c4687a5cb3d30f2f22917f1b128501eea.
-2022-06-25T02:02:03+00:00	INFO ::1	update	File Joomla_3.10.10-Stable-Update_Package.zip downloaded.
-2022-06-25T02:02:03+00:00	INFO ::1	update	Starting installation of new version.
-2022-06-25T02:02:09+00:00	INFO ::1	update	Finalising installation.
-2022-06-25T02:02:09+00:00	INFO ::1	update	Deleting removed files and folders.
-2022-06-25T02:02:10+00:00	INFO ::1	update	Cleaning up after installation.
-2022-06-25T02:02:10+00:00	INFO ::1	update	Update to version 3.10.10 is complete.
+2023-03-09T11:07:57+00:00	INFO ::1	update	Update started by user Super User (718). Old version is 3.10.10.
+2023-03-09T11:07:59+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.10.11-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20230309%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230309T110744Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=38acd1a6c63105cba6b5df894892146a56ce6e09a6c61aff32b1fe1d919efe59.
+2023-03-09T11:08:06+00:00	INFO ::1	update	File Joomla_3.10.11-Stable-Update_Package.zip downloaded.
+2023-03-09T11:08:06+00:00	INFO ::1	update	Starting installation of new version.
+2023-03-09T11:08:12+00:00	INFO ::1	update	Finalising installation.
+2023-03-09T11:08:12+00:00	INFO ::1	update	Deleting removed files and folders.
+2023-03-09T11:08:12+00:00	INFO ::1	update	Cleaning up after installation.
+2023-03-09T11:08:12+00:00	INFO ::1	update	Update to version 3.10.11 is complete.
